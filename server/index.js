8b1f229bad19659ba2f238fc541ad3273263a107
pankaj504 <120329170+pankaj504@users.noreply.github.com>
2024-06-18 18:59:41 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,8 @@ const { app, server } = require('./socket/index')
 
 // const app = express()
 app.use(cors({
-    origin : process.env.FRONTEND_URL,
+    origin : ["https://talktidefront.vercel.app/"],
+    methods : ["POST","GET"],
     credentials : true
 }))
 app.use(express.json())
